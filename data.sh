2a07ebf51fb365875053311be4e0747ddc300dfb
Krishnamohan <getin.mohan@gmail.com>
2021-06-26 16:59:14 +0000

    data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,11 @@
 hello
+<<<<<<< HEAD
 Hello I am from development brach from visakhapatnam
 
 Hello I am from development brach
 neet to upadate
 stash data need to update
+=======
+Hello I am from development brach
+neet to upadate
+>>>>>>> f8a75cd... updated
